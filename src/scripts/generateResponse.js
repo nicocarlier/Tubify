435abc2b158be449d222df848c865494ff14bd16
ncar285 <carlier.nicholas@gmail.com>
2023-11-17 18:29:40 +1300

    route to remotee server

--- src/scripts/generateResponse.js
+++ src/scripts/generateResponse.js
@@ -16,9 +16,9 @@ function titleize(str){
 function generateResponse(prompt, transcript, key){
     let title = key || "Text";
     title = titleize("Generated " + title)
-    // const passIn = prompt + transcript;
-    fetch(`http://localhost:5001`, {
-    // fetch("https://tubify-be02a8d8ea61.herokuapp.com/", {
+
+    // fetch(`http://localhost:5001`, {
+    fetch("https://tubify-be02a8d8ea61.herokuapp.com/", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -34,24 +34,16 @@ function generateResponse(prompt, transcript, key){
     })
     .then(data => {
         const markdownTextResponse = data.choices[0].message.content;
-        // const header = document.querySelector('.AI-output h2');
         const responseEle = document.querySelector('.ai-response');
         const aiBlock = document.querySelector('#AI-output');
         const loader = document.querySelector('#ai-loader');
         const gptLogo = document.querySelector('#gptlogo');
         const gptHolder = document.querySelector('.gptholder');
 
-        // debugger
-
-        // console.log(marked('**Bold**'));
-
-        
-        // console.log(title);
 
         const htmlContent = marked(markdownTextResponse);
         responseEle.innerHTML = htmlContent;
         
-        // header.innerHTML = title;
 
         //! hide loader:
         loader.classList.add('hidden');
